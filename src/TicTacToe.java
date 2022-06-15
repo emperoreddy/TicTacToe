2df3591c96a61cb7d781832ec63d3b46a29c6779
Amortitu Bogdan <emperoreddy@gmail.com>
2022-06-15 21:49:28 +0300

    first lines of code

---
+++ src/TicTacToe.java
@@ -0,0 +1,9 @@
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
+
