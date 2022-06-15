d5eda2ea9614c70866053a206162ef4cef93b93e
Amortitu Bogdan <emperoreddy@gmail.com>
2022-06-15 22:29:47 +0300

    Added grid and elements

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String input = scan.nextLine();
+        // convert String to char array
+
+        System.out.println("---------"); // upper grid
+
+        System.out.println("| " + input.charAt(0) + " " + input.charAt(1) + " "
+                + input.charAt(2) + " |");
+        System.out.println("| " + input.charAt(3) + " " + input.charAt(4) + " "
+                + input.charAt(5) + " |");
+        System.out.println("| " + input.charAt(6) + " " + input.charAt(7) + " "
+                + input.charAt(8) + " |");
+
+
+        System.out.println("---------"); // lower grid
     }
 }
 
